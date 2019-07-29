5dfb19544e9631ba5e20a4710f581797258c2f16
asmaur <maur.a@outlook.com>
2019-07-29 14:31:57 -0300

    ads build v1

--- frontads/src/components/MySubs.vue
+++ frontads/src/components/MySubs.vue
@@ -15,7 +15,7 @@
             </div>
 
             <div class="card-footer">
-                <button class="btn btn-primary btn-block ativar" @click="renovarPlano" :disabled="plano.membership.unpaid">Renovar </button>
+                <button class="btn btn-primary btn-block ativar" @click="renovarPlano" :disabled="plano.membership.renew">Renovar </button>
                 <button class="btn btn-primary btn-block disabled">Ativo</button>
             </div>
         </div>
